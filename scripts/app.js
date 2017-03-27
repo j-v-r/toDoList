bc0888e0cd321c7cf0b156b6c96dd4ca93aa931f
j-r <johnreardon@Johns-MacBook-Air-2.local>
2017-03-26 21:27:17 -0500

    To Do List Application 1.1
    Version 1.1 of To Do List Application
    
    Custom application allows user to make a basic list. Users can upload
    text via input field. After clicking the submit button the text will
    display below.
    
    Additional features allow users to delete items individually as well as
    sort their list chronologically.

--- scripts/app.js
+++ scripts/app.js
@@ -1,7 +1,10 @@
 const input = document.getElementById('input_value');
 const submit = document.getElementById('submit_button');
 const list = document.getElementById('list');
+const error = document.getElementById('error');
+const sortButton = document.getElementById('sort_button');
 const deleteButtons = document.getElementsByClassName('delete');
+const li = document.getElementsByTagName('li');
 
 // toDoList object
 
@@ -22,12 +25,39 @@ var toDoList = {
 						return deleteBttn;
 					},
 	displayTasks: function(listItems, tasks) {
-					for(i = 0; i < tasks.length; i++) {
+					for(var i = 0; i < tasks.length; i++) {
 						listItems[i].textContent = tasks[i];
 						listItems[i].appendChild(toDoList.deleteTaskButton());
 					}
+				},
+	removeTask: function() {
+					for(var i = 0; i < deleteButtons.length; i++) {
+						deleteButtons[i].addEventListener('click', (e) => {
+						e.target.parentElement.style.display = 'none';
+						// deleteTask();
+					})
 				}
+			}
+};
+
+// Error Message Controls
+
+function errorMessage() {
+	error.style.display = 'block';
+	input.className = 'error';
 }
+
+function removeErrorMessage() {
+	error.style.display = 'none';
+	input.className = '';
+}
+
+// function deleteTask() {
+// 	for(i = 0; i < toDoList.tasks; i++) {
+// 		var array = toDoList.tasks;
+// 		array.splice(i, array[i]);
+// 	}
+// }
 
 // Add task event handler
 submit.addEventListener('click', () => {
@@ -36,19 +66,50 @@ submit.addEventListener('click', () => {
 	let userInput = input.value;
 	let tasks = toDoList.tasks;
 
-	toDoList.addTask(userInput);
+	if(userInput === '') {
+		errorMessage();
+	}
 
-	toDoList.createListItem();
-	
-	toDoList.displayTasks(listItems, tasks);
-	
+	else {
+		removeErrorMessage();
+		toDoList.addTask(userInput);
+		toDoList.createListItem();
+		toDoList.displayTasks(listItems, tasks);
+		toDoList.removeTask();
+	}
+
+});
+
 // Remove task event handler
-	for( i = 0; i < deleteButtons.length; i++) {
-		deleteButtons[i].addEventListener('click', (e) => {
-			e.target.parentElement.style.display = 'none';
-		});
-}
-})
+// function removeTask() {
+// 		for(var i = 0; i < deleteButtons.length; i++) {
+// 		deleteButtons[i].addEventListener('click', (e) => {
+// 		e.target.parentElement.style.display = 'none';
+// 		});
+// 	}
+// }
+
+sortButton.addEventListener('click', (e) => { 
+	let sortHeading = document.getElementById('sort-heading');
+	let listItems = list.children;
+	let tasks = toDoList.tasks;
+	
+	li.reverse();
+	
+	// toDoList.tasks.reverse();
+	// toDoList.displayTasks(listItems, tasks);
+	// toDoList.removeTask();
+
+	if(sortHeading.textContent == 'First to Last') {
+		sortHeading.textContent = 'Last to First';
+	}
+	else {
+		sortHeading.textContent = 'First to Last'
+	}
+});
+
+
+
 
 
 
